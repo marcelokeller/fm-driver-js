37f214073dbd2d43d73c2abc8d13c9bb8415b131
Marcelo Keller <marcelo.keller@atlastechnol.com>
2025-04-16 15:57:03 -0300

    chore: add changes to package.json, remove clutter and add ci/cd release job

--- src/utilities.ts
+++ src/utilities.ts
@@ -1,9 +1,5 @@
 export function sendGAEvent(action: string, category = "Tour", label?: string) {
-  console.log(`event - action: ${action}, event_category: ${category}, event_label: ${label}`);
-
   if (window?.gtag && typeof window.gtag === "function") {
-     
-
     window.gtag("event", action, {
       event_category: category,
       event_label: label,
